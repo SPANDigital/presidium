9de5c5494f9ff313999ae5cd7254ae0bed06a198
Andres Felipe De Castro <af.decastro879@gmail.com>
2021-08-25 14:34:15 +0200

    PRS-1599 | Create static folder when executing init (#47)
    - Refactor Presidium site generator

--- pkg/domain/service/generator/generator.go
+++ pkg/domain/service/generator/generator.go
@@ -6,6 +6,9 @@ import (
 	"github.com/SPANDigital/presidium-hugo/pkg/domain/wizard"
 	"github.com/SPANDigital/presidium-hugo/pkg/log"
 	"github.com/spf13/viper"
+	"io/fs"
+	"os"
+	"path"
 )
 
 type Generator struct {
@@ -15,7 +18,28 @@ func New() Generator {
 	return Generator{}
 }
 
+// GenerateWithConfig generates a presidium site with the config given as parameter
+func (g Generator) GenerateWithConfig(c Config) error {
+	theTemplate, err := wizard.GetTemplate(c.Template)
+	if err != nil {
+		return err
+	}
+	tplSvc := template.New()
+	err = tplSvc.ProcessDirTemplates(theTemplate.Code(), c)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	err = os.Mkdir(path.Join(c.ProjectName, "static"), fs.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Generate uses viper to populate the default config and execute GenerateWithConfig
 func (g Generator) Generate() error {
+
 	themeKey := viper.GetString(config.ThemeKey)
 	theme, err := wizard.GetTheme(themeKey)
 	if err != nil {
@@ -25,20 +49,8 @@ func (g Generator) Generate() error {
 		Title:       viper.GetString(config.TitleKey),
 		ProjectName: viper.GetString(config.ProjectNameKey),
 		Theme:       theme.ModulePath(),
+		Template:    viper.GetString(config.TemplateNameKey),
 	}
 
-	templateKey := viper.GetString(config.TemplateNameKey)
-	theTemplate, err := wizard.GetTemplate(templateKey)
-	if err != nil {
-		log.Error(err)
-		return err
-	}
-
-	tplSvc := template.New()
-	err = tplSvc.ProcessDirTemplates(theTemplate.Code(), c)
-	if err != nil {
-		log.Error(err)
-		return err
-	}
-	return nil
+	return g.GenerateWithConfig(c)
 }
--- pkg/domain/service/generator/model.go
+++ pkg/domain/service/generator/model.go
@@ -4,4 +4,5 @@ type Config struct {
 	Title       string `json:"title"`
 	ProjectName string `json:"project_name"`
 	Theme       string `json:"theme"`
+	Template    string `json:"template"`
 }
