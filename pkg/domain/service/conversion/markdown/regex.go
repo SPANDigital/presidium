5994d4a24aa0c5392c9f70b37e8b228d4099d8d6
meyerhp <helgard.meyer@spandigital.com>
2022-01-19 10:19:35 +0200

    fix(tables): add titles to partial tables

--- pkg/domain/service/conversion/markdown/regex.go
+++ pkg/domain/service/conversion/markdown/regex.go
@@ -19,5 +19,5 @@ var IfConditionRe = regexp.MustCompile(`(?ms)(and|or)?\s??site.(\w+) ([!=]=) "(\
 var IfConditionShortcodeRe = regexp.MustCompile(`(?ms)site.(\w+) == "(\w+)"`)
 var ContainsShortcodeRe = regexp.MustCompile(`(?ms){{[%<].*?[%>]}}`)
 var CommentRe = regexp.MustCompile(`(?ms){% comment %}(.*?){% endcomment %}`)
-var TableBody = regexp.MustCompile(`(?:\|[^\n]+\|?\r?\n?){2,}`)
-var TableHeader = regexp.MustCompile(`(\|[^\n]+\|?\r?\n)((?:\|:?[-]+:?)+\|?)`)
+var TableBodyRe = regexp.MustCompile(`(?:\|[^\n]+\|?\r?\n?){2,}`)
+var TableHeaderRe = regexp.MustCompile(`(\|[^\n]+\|?\r?\n)?((?:\|:?\s*[-]+:?\s*)+\|?)`)
