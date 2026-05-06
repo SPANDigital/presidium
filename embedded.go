32fcb1337cbb2ff92ea1b6eec46a32940664e7c6
Matthew Moon <matthew_moon@apple.com>
2026-05-06 12:35:01 +0200

    added offline container test

--- embedded.go
+++ embedded.go
@@ -10,5 +10,6 @@ var templatesFS embed.FS
 
 // Embed themes as a compressed zip bundle
 // This avoids Go module embedding restrictions and improves extraction speed
+//
 //go:embed themes.zip
 var themesZip []byte
